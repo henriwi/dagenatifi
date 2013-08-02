60db4ba3c659e300291c54c523e9ce851d12187a
Henrik Wingerei <henrik.wingerei@bekk.no>
2013-08-02 10:55:18 +0200

    Skrevet om admin og score til ren angularjs. Slettet admin.js og score.js og lagt til app.js. Enkel versjon av score-siden med dropdown som setter aktivt event som cookie.

--- js/io.js
+++ js/io.js
@@ -4,17 +4,47 @@ var io = (function() {
 		{
 			school: "NTNU",
 			name: "Bedpress",
-			date: new Date("2013, 7, 31")
+			date: new Date("2013, 7, 31"),
+			participants: [
+				{
+					name: "Henrik",
+					phone: "45243706",
+					email: "henrikwingerei@gmail.com",
+					points: 167
+				},
+				{
+					name: "Ole",
+					phone: "99887766",
+					email: "ole.hansen@hioa.no",
+					points: 153
+				}
+			]
 		},
 		{
 			school: "UiO",
 			name: "Dagen at IFI",
-			date: new Date("2013, 7, 27")
+			date: new Date("2013, 7, 27"),
+			participants: [
+				{
+					name: "Petter",
+					phone: "12345678",
+					email: "petter@gmail.com",
+					points: 100
+				}
+			]
 		},
 		{
 			school: "HiOA",
 			name: "Næringslivsdagen",
-			date: new Date("2013, 4, 27")
+			date: new Date("2013, 4, 27"),
+			participants: [
+				{
+					name: "Hans",
+					phone: "22225555",
+					email: "hans@uio.no",
+					points: 58
+				}
+			]
 		}
 	];
 
@@ -33,11 +63,11 @@ var io = (function() {
 		});
 	}
 
-	function saveEvent(event, onSuccess, onError) {
+	function saveEvent(event) {
 		events.push(event);
 	}
 
-	function getEvents(onSuccess) {
+	function getEvents() {
 		return events;
 	}
 
